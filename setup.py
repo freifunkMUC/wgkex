2026d79705bb2f3402155bc9e5e7b43b9bdba368
Markus Hauschild <markus@moepman.eu>
2020-10-09 11:41:31 +0200

    make project installable via setuptools

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wireguard-tools",
+    version="0.1.0",
+    author="",
+    author_email="",
+    description="WireGuard Tools",
+    license="",
+    url="https://github.com/freifunkMUC/wireguard-tools",
+    packages=find_packages(exclude="tests"),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "Flask",
+        "PyYAML",
+        "voluptuous"
+    ],
+    setup_requires=["wheel"],
+    entry_points={
+        'console_scripts': [
+            'wgked=wgtools.frontend.wgked:main',
+        ],
+    }
+)
