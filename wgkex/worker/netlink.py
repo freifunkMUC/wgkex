bec9ebf539b0100c62de67e05524ce1ed09a0f4f
Annika Wickert <awlx@users.noreply.github.com>
2021-09-07 17:08:58 +0000

    Revert "use get_attr"
    This reverts commit bf324d2887c5ae30d55d3700e4dee810f8bb832e.

--- wgkex/worker/netlink.py
+++ wgkex/worker/netlink.py
@@ -182,11 +182,11 @@ def find_stale_wireguard_clients(wg_interface: str) -> List:
         clients = []
         infos = wg.info(wg_interface)
         for info in infos:
-            clients.extend(info.get_attr("WGDEVICE_A_PEERS"))
+            clients.extend(info.WGDEVICE_A_PEERS.value)
         ret = [
-            client.get_attr("WGPEER_A_PUBLIC_KEY").decode("utf-8")
+            client.WGPEER_A_PUBLIC_KEY.get("value", "").decode("utf-8")
             for client in clients
-            if client.get_attr("WGPEER_A_LAST_HANDSHAKE_TIME").get("tv_sec", int())
+            if client.WGPEER_A_LAST_HANDSHAKE_TIME.get("tv_sec", int())
             < three_hrs_in_secs
         ]
         return ret
