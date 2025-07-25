df184294cba140100e5de3730f734f1f8015be48
DasSkelett <git@dasskelett.dev>
2025-07-25 22:26:09 +0200

    parker: initial changes for Parker support (/api/v3)

--- wgkex/broker/app.py
+++ wgkex/broker/app.py
@@ -2,9 +2,11 @@
 """wgkex broker"""
 
 import dataclasses
+import ipaddress
 import json
 import re
-from typing import Any, Dict, Tuple
+from datetime import datetime, timezone
+from typing import Any, Dict, List, Tuple
 
 import paho.mqtt.client as mqtt_client
 from flask import Flask, Response, render_template, request
@@ -173,6 +175,144 @@ def wg_api_v2_key_exchange() -> Tuple[Response | Dict, int]:
     }
 
     return {"Endpoint": endpoint}, 200
+
+
+@app.route("/api/v3/wg/key/exchange", methods=["GET"])
+def wg_api_v3_key_exchange() -> Tuple[Response | Dict, int]:
+    @dataclasses.dataclass
+    class ParkerQuery:
+        v6mtu: int
+        pubkey: str  # client WG pubkey, base64 encoded
+        nonce: str
+
+        def __init__(self, v6mtu: int, pubkey: str, nonce: str) -> None:
+            self.v6mtu = v6mtu
+            self.pubkey = is_valid_wg_pubkey(pubkey)
+            self.nonce = nonce
+
+        @classmethod
+        def from_dict(cls, data: dict[str, Any]) -> "ParkerQuery":
+            """Creates a new Query object from dict."""
+            v6mtu: int = int(data.get("v6mtu", 1280))
+            pubkey: str = is_valid_wg_pubkey(data.get("pubkey", ""))
+            nonce: str = data.get("nonce", "")
+            return cls(v6mtu=v6mtu, pubkey=pubkey, nonce=nonce)
+
+    @dataclasses.dataclass
+    class ParkerResponse:
+        nonce: str
+        time: int  # current time as unix timestamp in seconds datetime.now(tz=timezone.utc).timestamp()
+        # NodeInfo data
+        # type NodeInfo struct {
+        # 	ID                    *uint64            `json:"id,omitempty" etcd:"id"`
+        # 	Concentrators         []ConcentratorInfo `json:"concentrators,omitempty" etcd:"-"`
+        # 	ConcentratorsJSON     []byte             `json:"-" etcd:"concentrators"`
+        # 	MTU                   *uint64            `json:"mtu,omitempty" etcd:"mtu"`
+        # 	Retry                 *uint64            `json:"retry,omitempty" etcd:"retry"`
+        # 	WGKeepalive           *uint64            `json:"wg_keepalive,omitempty" etcd:"wg_keepalive"`
+        # 	Range4                *string            `json:"range4,omitempty" etcd:"range4"`
+        # 	Range6                *string            `json:"range6,omitempty" etcd:"range6"`
+        # 	Address4              *string            `json:"address4,omitempty" etcd:"address4"`
+        # 	Address6              *string            `json:"address6,omitempty" etcd:"address6"`
+        # 	SelectedConcentrators *string            `json:"-" etcd:"selected_concentrators"`
+        # }
+
+        # type ConcentratorInfo struct {
+        # 	Address4 string `json:"address4"`
+        # 	Address6 string `json:"address6"`
+        # 	Endpoint string `json:"endpoint"`
+        # 	PubKey   string `json:"pubkey"`
+        # 	ID       uint32 `json:"id"`
+        # }
+
+        id: str
+        mtu: int
+        address6: str
+        concentrators: List[Dict[str, str | int]]
+        # selected_concentrators: This value contains a space separated list of the concentrator ids to
+        # include in the config response. If it is empty or not set, it will
+        # default to return all concentrators. Due to the implementation it is
+        # currently only possible to use concentrator ids between 1 and 64.
+        selected_concentrators: str
+        range6: str  # TODO take from IPAM
+        xlat_range6: str  # FFMUC addition
+        range4: str = "10.80.99.0/22"  # always the same with 464XLAT
+        address4: str = "10.80.99.1"
+        wg_keepalive: int = 25
+        retry: int = 120
+
+    try:
+        req_data = ParkerQuery.from_dict(request.args)
+    except Exception as ex:
+        logger.warning(
+            "Couldn't parse client query in /api/v3/wg/key/exchange: %s",
+            ex,
+            exc_info=True,
+        )
+        return {
+            "error": {
+                "message": "Invalid data received. Please check your request and try again."
+            }
+        }, 400
+
+    # TODO: fetch IPv6 /63 from Netbox, first /64 for the node's client network, second /64 for 464XLAT
+    # push both to gateways via MQTT, so that they configure the routing for it
+
+    full_range6 = get_range6(req_data.pubkey)
+    if full_range6 is None:
+        return {
+            "error": {
+                "message": "No IPv6 range available for this public key. Please try again later."
+            }
+        }, 500
+    ranges = full_range6.subnets(new_prefix=64)
+    range6 = ranges.__next__()
+    xlat_range6 = ranges.__next__()
+
+    gateway: str = "all"
+
+    mqtt_data = {
+        "PublicKey": req_data.pubkey,
+        "Range6": str(full_range6),
+        "Keepalive": None,  # TODO make configurable
+    }
+
+    logger.info(f"wg_api_v3_key_exchange: Key:{req_data.pubkey}")
+    mqtt.publish(f"parker/wireguard/{gateway}", json.dumps(mqtt_data).encode("utf-8"))
+
+    response = ParkerResponse(
+        nonce=req_data.nonce,
+        time=int(datetime.now(tz=timezone.utc).timestamp()),
+        id=req_data.pubkey,
+        mtu=min(req_data.v6mtu, 1375),
+        range6=str(range6),
+        xlat_range6=str(xlat_range6),
+        address6=str(range6.network_address + 1),
+        selected_concentrators="1",
+        concentrators=[
+            {
+                "address4": "10.0.0.1",
+                "address6": "fe80::28f:a7ff:fec6:7530",
+                "endpoint": f"{config.get_config().external_name}:40000",
+                "pubkey": "4WAyZBpHcyRE5+L4ApV+jjWgj4q1o3CrCQ3NjclXfV4=",
+                "id": 1,
+            }
+        ],
+    )
+
+    return dataclasses.asdict(response), 200
+
+
+def get_range6(pubkey: str) -> ipaddress.IPv6Network:
+    """Returns the IPv6 range for a node with a given public key."""
+    ranges = {
+        "7ybL85wUPWPktU25HvuPLsiDUIeqSR3Ydb3RuXpat1w=": "2001:db8:ed0:2::/63",
+        "ZoOtemmk3Ba62vYnzrcfHWTcfMWulRnvvvqwB8J2KmU=": "2001:db8:ed0:3::/63",
+    }
+    range = ranges.get(pubkey, None)
+    if range is None:
+        logger.error(f"No IPv6 range found for public key {pubkey}")
+    return ipaddress.IPv6Network(range)
 
 
 @mqtt.on_connect()
