d98221980785b04a5794e926abf33329ae34e0ce
DasSkelett <git@dasskelett.dev>
2025-07-25 22:26:09 +0200

    [WIP] Parker support (/api/v3)

--- wgkex/broker/app.py
+++ wgkex/broker/app.py
@@ -3,6 +3,7 @@
 import dataclasses
 import json
 import re
+from datetime import datetime, time, timezone
 from typing import Dict, Tuple, Any
 
 import paho.mqtt.client as mqtt_client
@@ -172,6 +173,109 @@ def wg_api_v2_key_exchange() -> Tuple[Response | Dict, int]:
     }
 
     return {"Endpoint": endpoint}, 200
+
+
+def wg_api_v3_key_exchange() -> Tuple[Response | Dict, int]:
+    @dataclasses.dataclass
+    class ParkerQuery:
+        v6mtu: int
+        pubkey: str  # client WG pubkey, base64 encoded
+        nonce: str
+
+        def __init__(self, v6mtu: int, pubkey: str, nonce: str) -> None:
+            self.v6mtu = v6mtu
+            self.pubkey = is_valid_wg_pubkey(pubkey)
+            self.nonce = nonce
+
+        @classmethod
+        def from_dict(cls, data: dict[str, Any]) -> "ParkerQuery":
+            """Creates a new Query object from dict."""
+            v6mtu: int = data.get("v6mtu", 1280)
+            pubkey: str = is_valid_wg_pubkey(data.get("pubkey", ""))
+            nonce: str = data.get("nonce", "")
+            return cls(v6mtu=v6mtu, pubkey=pubkey, nonce=nonce)
+
+    @dataclasses.dataclass
+    class ParkerResponse:
+        nonce: str
+        time: int  # current time as unix timestamp in seconds datetime.now(tz=timezone.utc).timestamp()
+        # NodeInfo data
+        # type NodeInfo struct {
+        # 	ID                    *uint64            `json:"id,omitempty" etcd:"id"`
+        # 	Concentrators         []ConcentratorInfo `json:"concentrators,omitempty" etcd:"-"`
+        # 	ConcentratorsJSON     []byte             `json:"-" etcd:"concentrators"`
+        # 	MTU                   *uint64            `json:"mtu,omitempty" etcd:"mtu"`
+        # 	Retry                 *uint64            `json:"retry,omitempty" etcd:"retry"`
+        # 	WGKeepalive           *uint64            `json:"wg_keepalive,omitempty" etcd:"wg_keepalive"`
+        # 	Range4                *string            `json:"range4,omitempty" etcd:"range4"`
+        # 	Range6                *string            `json:"range6,omitempty" etcd:"range6"`
+        # 	Address4              *string            `json:"address4,omitempty" etcd:"address4"`
+        # 	Address6              *string            `json:"address6,omitempty" etcd:"address6"`
+        # 	SelectedConcentrators *string            `json:"-" etcd:"selected_concentrators"`
+        # }
+
+        # type ConcentratorInfo struct {
+        # 	Address4 string `json:"address4"`
+        # 	Address6 string `json:"address6"`
+        # 	Endpoint string `json:"endpoint"`
+        # 	PubKey   string `json:"pubkey"`
+        # 	ID       uint32 `json:"id"`
+        # }
+
+        id: str
+        concentrators = [
+            {
+                "address4": "10.0.0.1",
+                "address6": "2001:bf7:381::1",
+                "endpoint": "46.226.127.236:10000",
+                "pubkey": "HxEDblNTIncBx3MiT535/JPbZDlnSdtC/+rEokOb0Hk=",
+                "id": 1,
+            }
+        ]
+        mtu: int
+        address6: str
+        # selected_concentrators: This value contains a space separated list of the concentrator ids to
+        # include in the config response. If it is empty or not set, it will
+        # default to return all concentrators. Due to the implementation it is
+        # currently only possible to use concentrator ids between 1 and 64.
+        selected_contentrators: str
+        range6: str  # TODO take from IPAM
+        range4: str = "10.80.99.0/22"  # always the same with 464XLAT
+        address4: str = "10.80.99.1"
+        wg_keepalive: int = 25
+        retry: int = 120
+
+    try:
+        req_data = ParkerQuery.from_dict(request.get_json(force=True))
+    except Exception as ex:
+        logger.error(
+            "Exception occurred in /api/v2/wg/key/exchange: %s", ex, exc_info=True
+        )
+        return {
+            "error": {
+                "message": "An internal error has occurred. Please try again later."
+            }
+        }, 400
+
+    # TODO: fetch IPv6 /63 from Netbox, first /64 for the node's client network, second /64 for 464XLAT
+    # push both to gateways via MQTT, so that they configure the routing for it
+
+    gateway: str = "all"
+
+    logger.info(f"wg_api_v3_key_exchange: Key:{req_data.pubkey}")
+    mqtt.publish(f"parker/wireguard/{gateway}", req_data.pubkey.encode("utf-8"))
+
+    response = ParkerResponse(
+        nonce=req_data.nonce,
+        time=int(datetime.now(tz=timezone.utc).timestamp()),
+        id=req_data.pubkey,
+        mtu=min(req_data.v6mtu, 1375),
+        range6="2001:db8:ed0:1::/64",
+        address6="2001:db8:ed0:1::1",
+        selected_contentrators="1",
+    )
+
+    return dataclasses.asdict(response), 200
 
 
 @mqtt.on_connect()
