42e938aa430fe66517380289ec88a7cce2e5505d
DasSkelett <git@dasskelett.dev>
2025-07-27 13:55:53 +0200

    Different topic prefixes for parker

--- wgkex/common/mqtt.py
+++ wgkex/common/mqtt.py
@@ -1,6 +1,9 @@
 """Common MQTT constants like topic string templates"""
 
-TOPIC_WORKER_WG_DATA = "wireguard-worker/{worker}/{domain}/data"
-TOPIC_WORKER_STATUS = "wireguard-worker/{worker}/status"
+# TODO only use parker prefix when parker is enabled
+TOPIC_WORKER_WG_DATA = "parker/wireguard-worker/{worker}/{domain}/data"
+TOPIC_WORKER_STATUS = "parker/wireguard-worker/{worker}/status"
 CONNECTED_PEERS_METRIC = "connected_peers"
-TOPIC_CONNECTED_PEERS = "wireguard-metrics/{domain}/{worker}/" + CONNECTED_PEERS_METRIC
+TOPIC_CONNECTED_PEERS = (
+    "parker/wireguard-metrics/{domain}/{worker}/" + CONNECTED_PEERS_METRIC
+)
